aba218362b480dc13c6a64b009fc1bda10981b63
Heather Berry <heatherberry@Heathers-MacBook-Air.local>
2017-04-24 17:54:06 -0400

    showing data but notafter I refresh

--- app/server.js
+++ app/server.js
@@ -1,14 +1,13 @@
 var bodyParser = require ('body-parser');
 var express = require('express');
 var path = require('path');
-var incomingPerson = [];
-
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }))
- 
 app.use(bodyParser.json())
+
+app.use(express.static('app/public'));
 
 require('../app/routing/apiRoutes.js')(app);
 require('../app/routing/htmlRoutes.js')(app);
@@ -16,9 +15,3 @@ require('../app/routing/htmlRoutes.js')(app);
 app.listen(PORT, function(){
 	console.log("App listening on PORT:" + PORT);
 });
-
-app.post("/newUser", function(req,res){
-	incomingPerson.push(req.body) 
-})
-
-module.exports = incomingPerson;
