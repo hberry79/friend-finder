f28d6107afc6b1acb7382c245cc399f691ab0018
Heather Berry <heatherberry@Heathers-MacBook-Air.local>
2017-04-24 19:06:44 -0400

    more functionaility. clearing the form

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,12 @@
 var friendData = require('../data/friends.js');
 
-module.exports = function (app){
-	app.get('/api', function(req, res){
-		res.json(friendData);
-	});
+module.exports = function(app) {
+    app.get('/api', function(req, res) {
+        res.json(friendData);
+    });
 
-	app.post('/newUser', function (req,res){
-		friendData.push(req.body);
-	});
+    app.post('/newUser', function(req, res) {
+        var newUser = req.body;
+        friendData.push(newUser);
+    });
 }
